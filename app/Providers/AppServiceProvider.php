1eb919aac79b15a4489263afe810393d5894d39d
mcode-developer <developer@mcoding.mn>
2024-05-08 18:42:16 +0800

    e-commerce frontend

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('layouts.inc.frontend.header', function ($view) {
+            $categories = Category::where('status', '0')->get();
+            $view->with('categories', $categories);
+        });
     }
 }
